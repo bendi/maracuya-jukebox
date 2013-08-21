7864aabb1ab1139a1fe2c4c8368ffd30b769ad37
Marek Będkowski <marek@bedkowski.pl>
2013-08-21 23:21:51 +0200

    Made jshint happy :)

--- src/util/sockets.js
+++ src/util/sockets.js
@@ -2,131 +2,132 @@ var TrackDao = require('../db/dao/TrackDao'),
   PlayController = require("./playController");
 
 function socketConnected(playController, mBus, socket) {
-	function emit() {
-	  mBus.emit.apply(mBus, arguments);
-	  socket.emit.apply(socket, arguments);
-	  socket.broadcast.emit.apply(socket, arguments);
-	}
+  function emit() {
+    mBus.emit.apply(mBus, arguments);
+    socket.emit.apply(socket, arguments);
+    socket.broadcast.emit.apply(socket, arguments);
+  }
 
-	/**
-	 * @param msg {String}
-	 * @param err {Object} Default null
-	 */
-	function error(msg, err) {
-	  socket.emit('error', msg, err);
-	}
 
-	function play(track) {
-	  if (!track) {
-	    console.log("No more tracks in the list.");
-	    return;
-	  }
-	  emit('play', playController.play(track));
+  /**
+   * @param msg {String}
+   * @param err {Object} Default null
+   */
+  function error(msg, err) {
+    socket.emit('error', msg, err);
+  }
 
-	  playController
-	    .removeAllListeners('end')
-	    .once('end', playNext);
-	}
+  function play(track) {
+    if (!track) {
+      console.log("No more tracks in the list.");
+      return;
+    }
+    emit('play', playController.play(track));
 
-	function playNext(currentId) {
-	  currentId = currentId || playController.currentId();
-	  emit('done');
-	  TrackDao.findNext(currentId)
-	    .success(play)
-	    .error(function(err) {
-	      error("Error when fetching track", err);
-	    });
-	}
+    playController
+      .removeAllListeners('end')
+      .once('end', playNext);
+  }
 
-	function playPrev(currentId) {
-	  currentId = currentId || playController.currentId();
-	  emit('done');
-	  TrackDao.findPrev(currentId)
-	    .success(play)
-	    .error(function(err) {
-	      error("Error when fetching track", err);
-	    });
-	}
+  function playNext(currentId) {
+    currentId = currentId || playController.currentId();
+    emit('done');
+    TrackDao.findNext(currentId)
+      .success(play)
+      .error(function(err) {
+        error("Error when fetching track", err);
+      });
+  }
 
-	socket.emit('connected', {
-	  currentlyPlaying: playController.getCurrentlyPlaying(),
-	  playlistId: 1,
-	  volume: playController.getVolume(),
-	  mute: playController.isMute
-	});
+  function playPrev(currentId) {
+    currentId = currentId || playController.currentId();
+    emit('done');
+    TrackDao.findPrev(currentId)
+      .success(play)
+      .error(function(err) {
+        error("Error when fetching track", err);
+      });
+  }
 
-	socket.on('play', function (data) {
-	  TrackDao.getById(data.id)
-	    .success(play)
-	    .error(function(err) {
-	      error("Error when fetching track", err);
-	    });
-	});
+  socket.emit('connected', {
+    currentlyPlaying: playController.getCurrentlyPlaying(),
+    playlistId: 1,
+    volume: playController.getVolume(),
+    mute: playController.isMute
+  });
 
-	socket.on('next', function (data) {
-	  if (!playController.isPlaying()) {
-	    return error('Cannot run next when not playing.');
-	  }
-	  playNext();
-	});
+  socket.on('play', function (data) {
+    TrackDao.getById(data.id)
+      .success(play)
+      .error(function(err) {
+        error("Error when fetching track", err);
+      });
+  });
 
-	socket.on('prev', function (data) {
-	  if (!playController.isPlaying()) {
-	    return error("Cannot run prev when not playing.");
-	  }
-	  playPrev();
-	});
+  socket.on('next', function (data) {
+    if (!playController.isPlaying()) {
+      return error('Cannot run next when not playing.');
+    }
+    playNext();
+  });
 
-	socket.on('stop', function (data) {
-	  try {
-	    playController.stop();
-	    emit('stop');
-	  } catch(e) {
-	    error(e);
-	  }
-	});
+  socket.on('prev', function (data) {
+    if (!playController.isPlaying()) {
+      return error("Cannot run prev when not playing.");
+    }
+    playPrev();
+  });
 
-	socket.on('pause', function (data) {
-	  try {
-	    playController.pause();
-	    emit('pause');
-	  } catch(e) {
-	    error(e);
-	  }
-	});
+  socket.on('stop', function (data) {
+    try {
+      playController.stop();
+      emit('stop');
+    } catch(e) {
+      error(e);
+    }
+  });
 
-	socket.on('resume', function (data) {
-	  try {
-	    var currentlyPlaying = playController.resume();
-	    console.log("emitting resume");
-	    emit('resume', currentlyPlaying);
-	  } catch(e) {
-	    error(e);
-	  }
-	});
+  socket.on('pause', function (data) {
+    try {
+      playController.pause();
+      emit('pause');
+    } catch(e) {
+      error(e);
+    }
+  });
 
-	socket.on('jump', function(percent) {
-	  try {
-	    var jumpSeconds = playController.jump(percent);
-	    emit('jump', jumpSeconds);
-	  } catch(e) {
-	    error(e);
-	  }
-	});
+  socket.on('resume', function (data) {
+    try {
+      var currentlyPlaying = playController.resume();
+      console.log("emitting resume");
+      emit('resume', currentlyPlaying);
+    } catch(e) {
+      error(e);
+    }
+  });
 
-	socket.on('volume', function(percent) {
-	  playController.volume(percent);
-	  socket.broadcast.emit('volume', percent);
-	});
+  socket.on('jump', function(percent) {
+    try {
+      var jumpSeconds = playController.jump(percent);
+      emit('jump', jumpSeconds);
+    } catch(e) {
+      error(e);
+    }
+  });
 
-	socket.on('mute', function() {
-	  playController.mute();
-	  emit('mute');
-	});
+  socket.on('volume', function(percent) {
+    playController.volume(percent);
+    socket.broadcast.emit('volume', percent);
+  });
 
-	socket.on('unmute', function() {
-	  playController.unmute();
-	  emit('unmute');
+  socket.on('mute', function() {
+    playController.mute();
+    emit('mute');
+  });
+
+  socket.on('unmute', function() {
+    playController.unmute();
+    emit('unmute');
     });
 }
 
