b2410c9c068f4679ce1a20eb258e8054a93fd1cb
Marek Bedkowski <marek@bedkowski.pl>
2014-02-28 21:23:02 +0000

    Added missing maracuya integration files

---
+++ distrib/epaper/deb-src/sysroot/opt/maracuya/epaper/epaper.js
@@ -0,0 +1,24 @@
+#!/opt/node/bin/node
+
+var	qrCode = require("qrcode-npm"),
+    exec = require("child_process").exec;
+
+//var text = "I want it all!";
+var text = process.argv[2];
+
+var qr = qrCode.qrcode(4, 'M');
+qr.addData(text);
+qr.make();
+
+var img = qr.createImgTag(4);    // creates an <img> tag as text
+
+img = img.replace(/^.*?src="(.*?)".*$/, "$1");
+
+var child = exec("sudo python "+__dirname+"/epaper.py '" + img + "'",function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
+
