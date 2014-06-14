0f44bfe5af6aa929ef5b94eb623c4aacecf8ce7c
Marek Będkowski <marek@bedkowski.pl>
2014-06-14 22:56:59 +0200

    Added config scripts for simple AP setup with zd1211 firmware.

---
+++ distrib/wifi/build.sh
@@ -0,0 +1,41 @@
+
+#!/usr/bin/env bash
+
+# IMPORTANT 
+# Protect agaisnt mispelling a var and rm -rf /
+set -u
+set -e
+
+NODE_VERSION=`node -v`
+
+ARCH=`dpkg --print-architecture`
+
+VERSION=$1
+
+APP_NAME=maracuya-jukebox-wifi-${VERSION}-${ARCH}
+SRC=/tmp/${APP_NAME}
+SYSROOT=${SRC}/
+DEBIAN=${SRC}/DEBIAN
+
+echo "Building Maracuya Jukebox wifi ap support for arch: $ARCH, version: $VERSION"
+
+rm -rf ${SRC}
+rsync -a deb-src/DEBIAN ${SRC}/
+rsync -a deb-src/sysroot/* ${SRC}/
+
+let SIZE=`du -s ${SYSROOT} | sed s'/\s\+.*//'`+8
+
+sed s"/\$SIZE/${SIZE}/" -i ${DEBIAN}/control
+sed s"/\$VERSION/${VERSION}/" -i ${DEBIAN}/control
+sed s"/\$ARCH/${ARCH}/" -i ${DEBIAN}/control
+
+chmod 755 ${DEBIAN}/*
+ 
+pushd /tmp/${APP_NAME}
+echo 2.0 > ${DEBIAN}/debian-binary
+popd
+
+fakeroot dpkg-deb --build /tmp/${APP_NAME}
+
+rsync -a /tmp/${APP_NAME}.deb ./
+
