78c643a5f9d796c1a3bec24f6cc0e6060865d00f
Marek Bedkowski <marek@bedkowski.pl>
2014-07-27 21:44:37 +0000

    Added packages fixes

--- epaper/install_epaper.sh
+++ epaper/install_epaper.sh
@@ -4,19 +4,21 @@
 # hopefully I got it correct from my history :)
 #
  
-apt-get install libi2c-dev
+apt-get install -y libi2c-dev
 #  not sure if this relevant at all - it was late and I was trying a lot of different things 
 # i2cdetect -y 1
-apt-get install i2c-tools
+apt-get install -y i2c-tools
 git clone git://git.drogon.net/wiringPi
 cd wiringPi/
 ./build
 
 # python package manager
 pushd /tmp
-curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
 popd
+
+apt-get install -y python-dev
 
 # and now python bindings
 pip install epyper
